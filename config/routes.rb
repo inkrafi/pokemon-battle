84c98b7c458318a6c14c74f3cc8478bbb1003fb6
inkrafi <rafiferdian7@gmail.com>
2023-12-29 08:23:25 +0700

    pokedex

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get  '/about', to: 'static_pages#about'
   post '/pokemon_battles/attack',  to: 'pokemon_battles#attack'
   get  '/pokemon_battles/history', to: 'pokemon_battles#history'
-  resources :pokedexes, only: [:index, :show]
+  resources :pokedexes#, only: [:index, :show]
   resources :pokemons
   resources :pokemon_battles
   resource  :pokemon_battle_details
